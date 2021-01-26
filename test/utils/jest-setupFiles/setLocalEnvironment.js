2e60f4b6ba544adc92a9b09547fb06f17f1452b7
mdaum <daumisgod@gmail.com>
2021-01-26 18:29:31 -0500

    fix .gitignore and add test dir

---
+++ test/utils/jest-setupFiles/setLocalEnvironment.js
@@ -0,0 +1,16 @@
+// Dynamo
+process.env.DYNAMO_ENDPOINT = 'http://localhost:8000';
+
+// Service Discovery
+process.env.ENVIRONMENT = 'mock';
+
+// Redis
+process.env.REDIS_HOST = 'localhost';
+process.env.REDIS_PORT = '7379';
+
+//S3
+process.env.S3_ENDPOINT = 'http://localhost:4572';
+
+//Fake AWS Creds
+process.env.AWS_ACCESS_KEY_ID = '123';
+process.env.AWS_SECRET_KEY = 'xyz';
