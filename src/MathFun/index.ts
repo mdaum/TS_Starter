8c01979d11d178e9de0f02868087b66b4a6806a3
Max Daum <max.daum@jupiterone.com>
2022-06-09 14:36:05 -0400

    remove implementation

--- src/MathFun/index.ts
+++ src/MathFun/index.ts
@@ -1,11 +1,4 @@
+// addtorial(5) = 5+4+3+2+1 = 15
 export function addtorial(num: number): number {
-  if (num < 0) {
-    throw new Error('input must be >= 0');
-  }
-  let result = 0;
-  while (num > 0) {
-    result += num;
-    num--;
-  }
-  return result;
+  //unimplemented
 }
--- src/getAPokemon/index.ts
+++ src/getAPokemon/index.ts
@@ -1,23 +1,14 @@
 import got from 'got';
 import { GetPokemonResponse, PokemonEntity } from './types';
 
+// return pokemon by hitting pokeapi with the id provided as param
+// https://pokeapi.co/
+// if an id is not provided, generate a random one (must be >= 1)
 export async function getAPokemon(pokemonId?: number): Promise<PokemonEntity> {
-  pokemonId = pokemonId ? pokemonId : Math.floor(Math.random()) + 1;
-  let rawPokemon: GetPokemonResponse;
-  try {
-    const response = await got.get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
-    rawPokemon = JSON.parse(response.body);
-  } catch (err) {
-    console.error('uh oh! something bad happened');
-    throw err;
-  }
-  return rawPokemonToEntity(rawPokemon);
+    //unimplemented
 }
 
+// converts a GetPokemonResponse to a PokemonEntity
 function rawPokemonToEntity(rawPokemon: GetPokemonResponse): PokemonEntity {
-  return {
-    id: rawPokemon.id,
-    name: rawPokemon.name,
-    type: rawPokemon.types.map(({ type }) => type.name).join('/'),
-  };
+    //unimplemented
 }
