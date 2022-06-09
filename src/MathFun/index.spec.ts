ea692ad7cd31079f0f3aa4e83e61576e79d4dd85
Max Daum <max.daum@jupiterone.com>
2022-06-09 13:59:08 -0400

    don't use class for addtorial

--- src/MathFun/index.spec.ts
+++ src/MathFun/index.spec.ts
@@ -1,12 +1,13 @@
-import mathFun from './index';
+import { addtorial } from './';
+
 describe('mathFun#addtorial', () => {
   it('addtorial should return expected number when valid input is given', () => {
     let expected = 0;
     for (let i = 0; i < 10; i++) {
-      expect(mathFun.addtorial(i)).toBe((expected += i));
+      expect(addtorial(i)).toBe((expected += i));
     }
   });
   it('should throw error when valid input is given', () => {
-    expect(() => mathFun.addtorial(-1)).toThrow('input must be >= 0');
+    expect(() => addtorial(-1)).toThrow('input must be >= 0');
   });
 });
--- src/MathFun/index.ts
+++ src/MathFun/index.ts
@@ -1,15 +1,11 @@
-class MathFun {
-  addtorial(num: number): number {
-    if (num < 0) {
-      throw new Error('input must be >= 0');
-    }
-    let result = 0;
-    while (num > 0) {
-      result += num;
-      num--;
-    }
-    return result;
+export function addtorial(num: number): number {
+  if (num < 0) {
+    throw new Error('input must be >= 0');
   }
+  let result = 0;
+  while (num > 0) {
+    result += num;
+    num--;
+  }
+  return result;
 }
-
-export default new MathFun();
